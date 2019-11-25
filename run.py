a9f9454d5621d184dee4bcccb868513f50501e88
John Elgin <voxter.256@gmail.com>
2019-11-24 22:52:29 -0500

    updated bot children

--- run.py
+++ run.py
@@ -1,18 +1,19 @@
 import json
 
-from bot.SantaBot import SantaBot
+from bot.LambdaSantaBot import LambdaSantaBot
+from bot.LocalSantaBot import LocalSantaBot
 from bot.Base import Base, engine
 
 
 if __name__ == '__main__':
 
-    bot = SantaBot()
+    bot = LocalSantaBot()
     Base.metadata.create_all(engine)
     bot.main()
 
 def lambda_handler(event, context):
     
-    bot = SantaBot()
+    bot = LambdaSantaBot()
     Base.metadata.create_all(engine)
     
     bot.process_message(event)
