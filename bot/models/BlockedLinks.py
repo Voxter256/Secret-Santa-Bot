d465fccabd6a9123d5e6be46d955b4534b4376e4
ainen <ainen@users.noreply.github.com>
2020-06-01 10:23:08 -0400

    Formatting

--- bot/models/BlockedLinks.py
+++ bot/models/BlockedLinks.py
@@ -10,10 +10,10 @@ class BlockedLink(Base):
     id = Column(Integer(), primary_key=True)
     participant_id = Column(Integer, ForeignKey('participants.id'), index=True)
     blocked_username = Column(String(32), index=True, nullable=True)
-    blocked_id = Column(Integer, ForeignKey(
-        'participants.id'), index=True, nullable=True)
+    blocked_id = Column(Integer,
+        ForeignKey('participants.id'), index=True, nullable=True)
 
-    blocker = relationship("Participant", uselist=False, backref=backref(
-        'blocker_link'), foreign_keys=participant_id)
-    blocked = relationship("Participant", uselist=False, backref=backref(
-        'blocked_link'), foreign_keys=blocked_id)
+    blocker = relationship("Participant", uselist=False,
+        backref=backref('blocker_link'), foreign_keys=participant_id)
+    blocked = relationship("Participant", uselist=False, 
+        backref=backref('blocked_link'), foreign_keys=blocked_id)
--- bot/models/Link.py
+++ bot/models/Link.py
@@ -9,12 +9,12 @@ class Link(Base):
 
     id = Column(Integer(), primary_key=True)
     santa_id = Column(Integer, ForeignKey('participants.id'), index=True)
-    receiver_id = Column(Integer, ForeignKey(
-        'participants.id'), index=True, default=None)
+    receiver_id = Column(Integer,
+        ForeignKey('participants.id'), index=True, default=None)
     group_id = Column(Integer, ForeignKey('groups.id'), index=True)
 
     santa = relationship("Participant", uselist=False,
                          backref=backref('link_santa'), foreign_keys=santa_id)
-    receiver = relationship("Participant", uselist=False, backref=backref(
-        'link_receiver'), foreign_keys=receiver_id)
+    receiver = relationship("Participant", uselist=False, 
+        backref=backref('link_receiver'), foreign_keys=receiver_id)
     groups = relationship("Group", uselist=False, backref=backref('links'))
